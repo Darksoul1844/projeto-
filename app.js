159bd79bdc5f4d74d4e25d5925dc29db452961da
Darksoul1844 <joao.paula24@escola.pr.gov.br>
2025-10-14 14:15:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let carrinho = [];
+let total = 0;
 
+function adicionarAoCarrinho(nome, preco) {
+    carrinho.push({ nome, preco });
+    total += preco;
+
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+    const lista = document.getElementById("lista-carrinho");
+    const totalSpan = document.getElementById("total");
+
+    lista.innerHTML = "";
+
+    carrinho.forEach((item, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${item.nome} - R$ ${item.preco},00`;
+        lista.appendChild(li);
+    });
+
+    totalSpan.textContent = total;
+}
